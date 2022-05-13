49edc203fa0693e53ab49a54117c95884db913f4
Nebroka <55884487+Nebroka@users.noreply.github.com>
2022-05-13 18:25:47 +0200

    Add WIP files via upload
    (all files still wip)

---
+++ Kennzeichen.java
@@ -0,0 +1,40 @@
+/**
+ * <p>Qualitaets- und UnterstuetzungsAgentur - Landesinstitut fuer Schule, Materialien zum schulinternen Lehrplan Informatik SII</p>
+ *
+ * @version 2014-03-13
+ */
+public class Kennzeichen implements ComparableContent<Kennzeichen> {
+    private String bezeichn;
+    private String ortskennz;
+
+    public Kennzeichen(String pBezeichn, String pOrtskennz) {
+        super();
+        bezeichn = pBezeichn;
+        ortskennz = pOrtskennz;
+    }
+
+    public String gibBezeichnung(){
+        return bezeichn;
+    }
+
+    public String gibOrtskz() {
+        return ortskennz;
+    }
+
+    public boolean isLess(Kennzeichen pContent) {
+        return this.gibBezeichnung().compareTo(pContent.gibBezeichnung())<0;
+    }
+
+    public boolean isEqual(Kennzeichen pContent) {
+        return this.gibBezeichnung().compareTo(pContent.gibBezeichnung())==0;
+    }
+
+    public boolean isGreater(Kennzeichen pContent) {
+        return this.gibBezeichnung().compareTo(pContent.gibBezeichnung())>0;
+    }
+
+    public String toString() {
+        return ortskennz + " *" + bezeichn;
+    }
+
+}
---
+++ Kennzvw.java
@@ -0,0 +1,61 @@
+/**
+ * <p>Qualitaets- und UnterstuetzungsAgentur - Landesinstitut fuer Schule, Materialien zum schulinternen Lehrplan Informatik SII</p>
+ *
+ * @version 2014-03-13
+ */
+public class Kennzvw {
+    private BinarySearchTree<Kennzeichen> baum;
+
+    public Kennzvw() {
+        baum = new BinarySearchTree<Kennzeichen>();
+    }
+
+    public void ergaenze(String pBezeichn, String pOrtskennz) {
+        Kennzeichen kennz = new Kennzeichen(pBezeichn, pOrtskennz);
+        baum.insert(kennz);
+    }
+
+    public void loesche(String pBezeichn, String pOrtskennz) {
+        Kennzeichen kennz = new Kennzeichen(pBezeichn, pOrtskennz);
+        if (!baum.isEmpty()){
+            baum.remove(kennz);
+        }
+    }
+
+    public BinaryTree<Kennzeichen> gibBaum() {  
+        return gibBaum(baum);
+    }
+    private BinaryTree<Kennzeichen> gibBaum(BinarySearchTree<Kennzeichen> bs){
+        BinaryTree<Kennzeichen> b;
+        if(bs.isEmpty())
+            b=new BinaryTree<Kennzeichen>();
+        else {b=new BinaryTree<Kennzeichen>(bs.getContent());   
+            if(bs.getLeftTree()!=null)
+                b.setLeftTree(this.gibBaum(bs.getLeftTree()));
+            if(bs.getRightTree()!=null)
+                b.setRightTree(this.gibBaum(bs.getRightTree()));   
+        }  
+        return b;     
+    }
+
+    public void leereBaum() {  
+        baum = new BinarySearchTree<Kennzeichen>(); 
+    }
+
+    public String toString() {
+        return inorder(baum);
+    }
+
+    public String inorder(BinarySearchTree<Kennzeichen> b) {
+        String links = "";
+        String mitte = "";
+        String rechts = "";
+        if (!b.isEmpty()) {
+            links = inorder(b.getLeftTree());
+            mitte = b.getContent().toString()+"\n";
+            rechts = inorder(b.getRightTree());
+        }  
+        return links + mitte + rechts;
+    }
+
+}
